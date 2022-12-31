b5039a65caa1cc76f0032e90d2b3a2f7a2123903
gianlucanadir <gianlucanadir@gmail.com>
2022-12-31 14:43:16 +0100

    test

--- src/hash.js
+++ src/hash.js
@@ -8,13 +8,13 @@ function hash(input) {
 
 // Compare two hashed passwords
 
-let password = 'hi-mom!';
+let password = 'hello';
 const hash1 = hash(password);
 console.log(hash1)
 
 /// ... some time later
 
-password = 'hi-mom!';
+password = 'hello!';
 const hash2 = hash(password);
 const match = hash1 === hash2;
 
